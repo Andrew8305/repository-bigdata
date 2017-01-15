2d47342c9c6aeb31023bca1b6a1c6d32851feb9d
wulin <125906088@qq.com>
2017-01-15 13:54:29 +0800

    data plat project code modify commit

--- dataplat/src/main/java/org/platform/dataplat/utils/bigdata/HadoopUtils.java
+++ dataplat/src/main/java/org/platform/dataplat/utils/bigdata/HadoopUtils.java
@@ -12,23 +12,21 @@ public class HadoopUtils extends AbstrUtils {
 	public static final String HDFS_USER_WAREHOUSE = "hdfs://centos.master:9000/user/Administrator";
 	
 	public static FileSystem getFileSystem() {
-		FileSystem fileSystem = null;
 		try {
-			fileSystem = FileSystem.get(URI.create(HDFS_DATA_WAREHOUSE), configuration);
+			return FileSystem.get(URI.create(HDFS_DATA_WAREHOUSE), configuration);
 		} catch (IOException e) {
 			LOG.info(e.getMessage(), e);
 		}
-		return fileSystem;
+		return null;
 	}
 	
 	public static FileSystem getFileSystem(String hdfsPath) {
-		FileSystem fileSystem = null;
 		try {
-			fileSystem = FileSystem.get(URI.create(hdfsPath), configuration);
+			return FileSystem.get(URI.create(hdfsPath), configuration);
 		} catch (IOException e) {
 			LOG.info(e.getMessage(), e);
 		}
-		return fileSystem;
+		return null;
 	}
 
 }
